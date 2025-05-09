6005e3331ab18176760d093e5eddbc92df16c4fa
Jackson Fitzgerald <jackson@hedgepayments.com>
2025-05-09 01:03:22 -0400

    Add Hedge Payments logo as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,25 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Hedge Payments',
   description: 'Payments, made fun.',
+  icons: {
+    icon: [
+      {
+        url: '/favicon/favicon.ico',
+        sizes: '16x16',
+        type: 'image/x-icon',
+      },
+      {
+        url: '/favicon/logo-icon.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+    ],
+    apple: {
+      url: '/favicon/hedge-logo.svg',
+      sizes: '180x180',
+      type: 'image/svg+xml',
+    },
+  },
 }
 
 export default function RootLayout({
