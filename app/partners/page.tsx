9757c6653e28722860ce0c324996c1e512cf03c7
jacksonhedge <jackson@hedgepayments.com>
2025-05-19 14:12:09 -0400

    Add partner logos page and components

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -4,11 +4,11 @@ import styles from '../page.module.css';
 
 export default function PartnersPage() {
   const partnerLogos = [
-    { src: '/images/assets/partner1.png', alt: 'Partner 1' },
-    { src: '/images/assets/partner2.png', alt: 'Partner 2' },
-    { src: '/images/assets/partner3.png', alt: 'Partner 3' },
-    { src: '/images/assets/partner4.png', alt: 'Partner 4' },
-    { src: '/images/assets/partner5.png', alt: 'Partner 5' },
+    { src: '/images/assets/mgm.svg', alt: 'MGM' },
+    { src: '/images/assets/draftkings.svg', alt: 'DraftKings' },
+    { src: '/images/assets/fanatics.svg', alt: 'Fanatics' },
+    { src: '/images/assets/fanduel.svg', alt: 'FanDuel' },
+    { src: '/images/assets/pokerstars.svg', alt: 'PokerStars' },
   ];
 
   return (
